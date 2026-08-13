107a9e53370e54875997bd557d5f8e613ad59823
buchiman12 <cbuchiman@proton.me>
2026-08-13 20:03:03 +0100

    practicing

---
+++ manifest.js
@@ -0,0 +1,21 @@
+let manifestList = {
+  containerId: 0,
+  destination: "",
+  weight: 0,
+  unit: "",
+  hazmat: true,
+}
+
+let normalizeUnits = (manifest) => {
+  return manifest.containerId = 68, manifest.destination = "Salinas", manifest.weight = 45.45,
+  manifest.unit = "kg";
+
+}
+let validateManifest = (manifest) => {
+
+}
+
+let processManifest = (manifest) => {
+
+}
+console.log(normalizeUnits(manifestList.containerId))
